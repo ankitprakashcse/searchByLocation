860ba375cd01ed8084ed71a2dc5881f9dc0d5709
Ankit Prakash <ankitprakash.cse@gmail.com>
2019-07-16 12:22:51 +0530

    added build files

---
+++ build/precache-manifest.8a871f2300edfea01907709b86b3e325.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af129d62e92d38925b728dacad1ece29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616ce2abacd68e5ce872",
+    "url": "/static/css/main.c1a6518a.chunk.css"
+  },
+  {
+    "revision": "63b8c8742b0f9c6da541",
+    "url": "/static/js/2.157aece2.chunk.js"
+  },
+  {
+    "revision": "616ce2abacd68e5ce872",
+    "url": "/static/js/main.d0e36ba7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
